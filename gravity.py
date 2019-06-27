6c22ac07bc012b3a027b4678682241ac56a484ea
Sushanth Keshav <k.sushanth.k@gmail.com>
2019-06-27 16:47:52 +0200

    Task 3b : Pcolor mesh to analyze the Gravitational potential

--- gravity.py
+++ gravity.py
@@ -24,51 +24,65 @@ for position in POSITIONS:
     assert len(position) == number_of_dimensions
 for velocity in POSITIONS:
     assert len(velocity) == number_of_dimensions
-POSITIONS_STORE = [POSITIONS]
-mass = np.array([MASSES[1],MASSES[0]])
+
+#Defining the meshgrid such that particle masses are present at the centre of each pixels
+
+x_start=-1.5
+x_end=1.5
+y_start=-1.5
+y_end=1.5
+
+## Number of pixels i.e., little rectangles facilitating sharp visulaization
+
+n_pixel_x=10
+n_pixel_y=10
+
+dx=(x_end-x_start)/n_pixel_x
+dy=(y_end-y_start)/n_pixel_y
+
+xv=np.linspace(x_start,x_end,n_pixel_x+1)
+yv=np.linspace(y_start,y_end,n_pixel_y+1)
+
+xc=np.linspace(x_start+(dx/2),x_end-(dx/2),n_pixel_x)
+yc=np.linspace(y_start+(dy/2),y_end-(dy/2),n_pixel_y)
+
+xv_2d,yv_2d=np.meshgrid(xv,yv)
+xc_2d,yc_2d=np.meshgrid(xc,yc)
+
+
+mass = np.array([MASSES[1],MASSES[0]]) # This mass variable is used in computing the acceleration 
+
 for step in tqdm(range(NUMBER_OF_TIME_STEPS+1)):
+    #Computing Gravitational Potential
+    potential_sum = np.zeros_like(xc_2d)
+    for i in range(number_of_planets):
+        x1 = POSITIONS[i,0] - xc_2d
+        x1_2 = np.square(x1)
+        y1 = POSITIONS[i,1] - yc_2d
+        y1_2 = np.square(y1)
+        distance = x1_2 + y1_2
+        potential_sum += (-GRAVITATIONAL_CONSTANT*MASSES[i])*(np.reciprocal(np.sqrt(distance)))
+        
+
     # plotting every single configuration does not make sense
-	#if step % PLOT_INTERVAL == 0:
-	#	fig, ax = plt.subplots()
-	#	x = []
-	#	y = []
-	#	for position in POSITIONS:
-	#		x.append(position[0])
-	#		y.append(position[1])
-	#	ax.scatter(x, y)
-	#	ax.set_aspect("equal")
-	#	ax.set_xlim(-1.5, 1.5)
-	#	ax.set_ylim(-1.5, 1.5)
-	#	ax.set_title("t = {:8.4f} s".format(step * TIME_STEP))
-	#	ax.set_xlabel("x")
-	#	ax.set_ylabel("y")
-	#	output_file_path = Path("positions", "{:016d}.png".format(step))
-	#	output_file_path.parent.mkdir(exist_ok=True)
-	#	fig.savefig(output_file_path)
-	#	plt.close(fig)
-	
-    # the accelerations for each planet are required to update the velocities
+    if step % PLOT_INTERVAL == 0:
+        fig, ax = plt.subplots()
+        plt.pcolormesh(xv,yv,potential_sum)  #pcolormesh
+        plt.colorbar()
+        ax.set_aspect("equal")
+        ax.set_xlim(-1.5, 1.5)
+        ax.set_ylim(-1.5, 1.5)
+        ax.set_title("Potential at t = {:8.4f} s".format(step * TIME_STEP))
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        output_file_path = Path("potentials", "{:016d}.png".format(step))
+        output_file_path.parent.mkdir(exist_ok=True)
+        fig.savefig(str(output_file_path))
+        plt.close(fig)
     
     distance_vector = POSITIONS[0]-POSITIONS[1]
-    distance_vector_length = (np.linalg.norm(distance_vector))**2
+    distance_vector_length = (np.linalg.norm(distance_vector))**2  #Computing the Distance vector Length
     acceleration = (GRAVITATIONAL_CONSTANT*mass/distance_vector_length)*np.array([-1*distance_vector,distance_vector])
-	#Semi implicit Euler integration
-    VELOCITIES = VELOCITIES + acceleration * TIME_STEP #Getting updated velocity by v_new v_old + a_old*delta_t
-    POSITIONS = POSITIONS + VELOCITIES*TIME_STEP #getting updated position x_new = x_old + v_new*delta_t
-    POSITIONS_STORE.append(POSITIONS)
-
-
-#converting the POSITIONS list into array to facilitate plotting
-position_array = np.array(POSITIONS_STORE) #Shape will be (N,2,2)
-
-
-
-fig, ax = plt.subplots(ncols=2, figsize=(15,5))
-for i in range(0, position_array.shape[1]):
-    ax[i].plot(position_array[:, i, 0], position_array[:, i, 1]) #Slicing so as to plot X and Y 
-    ax[i].set_title(str('Trajectory of Planet:')+str(i+1), fontsize = 20)
-    ax[i].set_xlabel("x", fontsize=20)
-    ax[i].set_ylabel("y", fontsize = 20)
-    fig.savefig("Trajectotres_of_planets_Semi_Implicit_Euler.png")
-    plt.close(fig)
+    POSITIONS = POSITIONS + VELOCITIES*TIME_STEP #position update
+    VELOCITIES = VELOCITIES + acceleration * TIME_STEP #position update
 
